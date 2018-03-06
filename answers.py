b81e037f19b6654409f252189b1c4b7a611ab971
Amara Datta <amaradatta@gmail.com>
2018-03-06 00:04:26 -0800

    Organized answers

--- answers.py
+++ answers.py
@@ -8,26 +8,30 @@ def print_divider():
 
 
 LINK = 'https://en.wikipedia.org/wiki/National_Football_League'
-DIVISION = 'South'
 
 # Question 1 params
-STADIUM_MIN_SEATING = 50000
-STADIUM_MAX_SEATING = 80000
-
-# Question 2 Params
 LATITUDE = 39
 LONGITUDE = 84
 
-find_stadiums_within_seat_range(STADIUM_MIN_SEATING, STADIUM_MAX_SEATING, LINK)
+# Question 2 params
+DIVISION = 'South'
 
-print_divider()
+# Question 3 Params
+STADIUM_MIN_SEATING = 50000
+STADIUM_MAX_SEATING = 80000
 
+
+# Question 1
 find_teams_with_bounds(LATITUDE, LONGITUDE, LINK)
-
 print_divider()
 
+# Question 2
 find_teams_in_division(DIVISION, LINK)
-
 print_divider()
 
+# Question 3
+find_stadiums_within_seat_range(STADIUM_MIN_SEATING, STADIUM_MAX_SEATING, LINK)
+print_divider()
+
+# Question 4
 get_image_link(LINK)
